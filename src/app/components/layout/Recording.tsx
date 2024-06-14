53ba8693dfd3b89f7eb7be93811d07ca1950b815
Zheka Grem <odinked@gmail.com>
2024-06-14 15:25:26 +0300

    fix ts calendar

--- src/app/components/layout/Recording.tsx
+++ src/app/components/layout/Recording.tsx
@@ -1,25 +1,32 @@
 'use client'
 import React from 'react';
 import style from '@/app/assets/styles/recording.module.css';
-import FullCalendar from '@fullcalendar/react';
+import FullCalendar  from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import { EventClickArg } from '@fullcalendar/core';
 
 
 
-const Recording = () => {
-  const handleEventClick = (clickInfo) => {
-    clickInfo.jsEvent.preventDefault(); 
+const handleEventClick = (clickInfo: EventClickArg) => {
+  clickInfo.jsEvent.preventDefault();
+  const start = clickInfo.event.start;
+  const end = clickInfo.event.end;
+  if (start && end) {
     alert(
       `Подія: ${
         clickInfo.event.title
-      } Час: з ${clickInfo.event.start.getHours()}:${clickInfo.event.start.getMinutes()} до ${clickInfo.event.end.getHours()}:${clickInfo.event.start.getMinutes()}`
+      } Час: з ${start.getHours()}:${start.getMinutes()} до ${end.getHours()}:${end.getMinutes()}`
     );
-  };
+  } else {
+    alert(`Подія: ${clickInfo.event.title} Час: невідомий`);
+  }
+};
+const Recording: React.FC = () => {
   return (
     <section className={style.container} id="recording">
       <div className={style.calendar}>
-        <div className="calendarContainer">
+        
           <FullCalendar
             plugins={[dayGridPlugin, googleCalendarPlugin]}
             googleCalendarApiKey="AIzaSyBJA1zbivKJUmvc442zIMADpHMugKbDvX0"
@@ -39,18 +46,11 @@ const Recording = () => {
             firstDay={1}
             eventClick={handleEventClick}
           />
-        </div>
-        {/* <iframe
-          src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FKiev&bgcolor=%23ffffff&hl=uk&title=oniostudio&src=YzhlZmQ0NjE2NjAxNzIwZTQ2MzQ4NTNhNjE1MmVmOWI0OGVlMTg1YmM4MjkyZDhlYWRhZjg2NTUwNTY3MDViY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F09300"
-          style={{ border: 'solid 1px #777' }}
-          width="100%"
-          height="600"
-          scrolling="no"
-        ></iframe> */}
+        
+
       </div>
     </section>
   );
 };
 
 export default Recording;
-// tsubylia.records.official@gmail.com
