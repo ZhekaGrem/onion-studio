95e1f11f9d07a5bdc4f2e66cd31deb2cf88029d7
Zheka Grem <odinked@gmail.com>
2024-06-14 14:50:05 +0300

    add new caledar

--- src/app/components/layout/Aboutus.tsx
+++ src/app/components/layout/Aboutus.tsx
@@ -3,56 +3,106 @@ import React,{useState} from 'react'
 import style from '@/app/assets/styles/aboutus.module.css'
 import Image from 'next/image';
 import Contacts from '../forms/Contacts'
+import { motion, AnimatePresence } from 'framer-motion';
 
-const Aboutus = () => {
+type IndexType= number | null;
+
+type HandleClickButton=(index:number)=>void;
+
+type AboutusList={
+  key?: number;
+  minititle?:string
+  title:string;
+  info:string;
+
+}
+
+
+const aboutuslist: AboutusList[] = [
+  {
+    key: 1,
+    title: 'Доступні ціни',
+    info:
+      ' Over 700 purpose-built, 24-hour studios across the UK, US and DE. Join a community of 350k artists and access exclusive opportunities to perform live and get your songs played on the radio.',
+  },
+  {
+    key: 2,
+    title: 'Допомога в створенні контенту',
+    info:
+      ' Over 700 purpose-built, 24-hour studios across the UK, US and DE. Join a community of 350k artists and access exclusive opportunities to perform live and get your songs played on the radio.',
+  },
+  {
+    key: 3,
+    title: 'Гнучкі можливості скасування',
+    info:
+      ' Over 700 purpose-built, 24-hour studios across the UK, US and DE. Join a community of 350k artists and access exclusive opportunities to perform live and get your songs played on the radio.',
+  },
+  {
+    key: 3,
+    title: 'Допомога початківцям',
+    info:
+      ' Over 700 purpose-built, 24-hour studios across the UK, US and DE. Join a community of 350k artists and access exclusive opportunities to perform live and get your songs played on the radio.',
+  },
+];
+
+const Aboutus:React.FC = () => {
+   const [openIndex, setOpenIndex] = useState < IndexType>(null);
   const [isOrderOpen, setIsOrderOpen] = useState(false);
+  const handleButtonClick: HandleClickButton = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
   return (
     <>
-    <section id="aboutus" className={style.aboutSection}>
-      <div className={style.blurBg}>
-        <div className={style.container}>
-          <div className={style.containerInfo}>
-            <h4>чому варто обрати нас</h4>
-            <h2>Студії від художників, для художників</h2>
-            <p>
-              Over 700 purpose-built, 24-hour studios across the UK, US and DE. Join a community of 350k
-              artists and access exclusive opportunities to perform live and get your songs played on the
-              radio.
-            </p>
-          </div>
-          <div className={style.containerBenefits}>
-            <div>
-              <ul>
-                <li>
-                  <button>Доступні ціни</button>
-                </li>
-                <li>
-                  <button>Допомога в створенні контенту</button>
-                </li>
-                <li>
-                  <button>Гнучкі можливості скасування</button>
-                </li>
-                <li>
-                  <button>Допомога початківцям</button>
-                </li>
-              </ul>
+      <section id="aboutus" className={style.aboutSection}>
+        <div className={style.blurBg}>
+          <div className={style.container}>
+            <div className={style.containerInfo}>
+              <h4>чому варто обрати нас</h4>
+              <h2>Студії від художників, для художників</h2>
+              <p>
+                Over 700 purpose-built, 24-hour studios across the UK, US and DE. Join a community of 350k
+                artists and access exclusive opportunities to perform live and get your songs played on the
+                radio.
+              </p>
             </div>
-            <div>
-              <Image
-                className={style.containerBenefitsImg}
-                src="/011720NYRE-4201.jpg"
-                width={400}
-                height={400}
-                alt="kek"
+            <div className={style.containerBenefits}>
+              <div>
+                <ul>
+                  {aboutuslist.map((item, index) => (
+                    <li key={item.key}>
+                      <button onClick={() => handleButtonClick(index)}>{item.title}</button>
+                      <AnimatePresence>
+                        {openIndex === index && (
+                          <motion.div
+                            initial={{ height: 0, opacity: 0 }}
+                            animate={{ height: 'auto', opacity: 1 }}
+                            exit={{ height: 0, opacity: 0 }}
+                            transition={{ duration: 0.5 }}
+                          >
+                            <p>{item.info}</p>
+                          </motion.div>
+                        )}
+                      </AnimatePresence>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <Image
+                  className={style.containerBenefitsImg}
+                  src="/011720NYRE-4201.jpg"
+                  width={400}
+                  height={400}
+                  alt="kek"
                 />
+              </div>
             </div>
+            <button onClick={() => setIsOrderOpen(true)}> book a studio</button>
           </div>
-          <button onClick={() => setIsOrderOpen(true)}> book a studio</button>
         </div>
-      </div>
-    </section>
-    {isOrderOpen && <Contacts onClose={() => setIsOrderOpen(false)} />}
-                </>
+      </section>
+      {isOrderOpen && <Contacts onClose={() => setIsOrderOpen(false)} />}
+    </>
   );
 }
 
--- src/app/components/layout/Recording.tsx
+++ src/app/components/layout/Recording.tsx
@@ -1,20 +1,56 @@
+'use client'
 import React from 'react';
 import style from '@/app/assets/styles/recording.module.css';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+
+
 
 const Recording = () => {
+  const handleEventClick = (clickInfo) => {
+    clickInfo.jsEvent.preventDefault(); 
+    alert(
+      `Подія: ${
+        clickInfo.event.title
+      } Час: з ${clickInfo.event.start.getHours()}:${clickInfo.event.start.getMinutes()} до ${clickInfo.event.end.getHours()}:${clickInfo.event.start.getMinutes()}`
+    );
+  };
   return (
     <section className={style.container} id="recording">
-      <div>
-        <iframe
+      <div className={style.calendar}>
+        <div className="calendarContainer">
+          <FullCalendar
+            plugins={[dayGridPlugin, googleCalendarPlugin]}
+            googleCalendarApiKey="AIzaSyBJA1zbivKJUmvc442zIMADpHMugKbDvX0"
+            events={{
+              googleCalendarId: 'tsubylia.records.official@gmail.com',
+            }}
+            initialView="dayGridMonth"
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,dayGridWeek,dayGridDay',
+              
+            }}
+         
+            height="auto"
+            locale="uk"
+            firstDay={1}
+            eventClick={handleEventClick}
+          />
+        </div>
+        {/* <iframe
           src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FKiev&bgcolor=%23ffffff&hl=uk&title=oniostudio&src=YzhlZmQ0NjE2NjAxNzIwZTQ2MzQ4NTNhNjE1MmVmOWI0OGVlMTg1YmM4MjkyZDhlYWRhZjg2NTUwNTY3MDViY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F09300"
           style={{ border: 'solid 1px #777' }}
           width="100%"
           height="600"
           scrolling="no"
-        ></iframe>
+        ></iframe> */}
       </div>
     </section>
   );
 };
 
 export default Recording;
+// tsubylia.records.official@gmail.com
